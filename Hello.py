3b4d4481e188d676b2d7438a8f65aae0b646d914
Ayomitunde Fafore <ayomitunde.fafore@snowflake.com>
2023-11-21 09:50:30 -0600

    update

--- Hello.py
+++ Hello.py
@@ -1,24 +1,31 @@
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
+import snowflake.connector
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 
 
 def run():
-    # with open("./dataset-credentials.p8", "rb") as key:
-    #     p_key = serialization.load_pem_private_key(
-    #         key.read(),
-    #         password=None,
-    #         backend=default_backend()
-    #     )
-    #
-    # pkb = p_key.private_bytes(
-    #     encoding=serialization.Encoding.DER,
-    #     format=serialization.PrivateFormat.PKCS8,
-    #     encryption_algorithm=serialization.NoEncryption())
-    conn = st.connection("snowflake")
+    with open("./dataset-credentials.p8", "rb") as key:
+        p_key = serialization.load_pem_private_key(
+            key.read(),
+            password=None,
+            backend=default_backend()
+        )
+
+    pkb = p_key.private_bytes(
+        encoding=serialization.Encoding.DER,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption())
+
+    conn = snowflake.connector.connect(
+        user=st.secrets["user"],
+        account=st.secrets["account"],
+        private_key=pkb,
+    )
+    print("connected to snowflake!")
+    cur = conn.cursor()
     # conn = st.experimental_connection("snowpark", private_key=pkb, role="readonly_role")
-    query = conn.query('select * from free_dataset_GZ1M6Z2R41Y.public.t_rbaseit limit 10;');
+    query = cur.execute('select * from free_dataset_GZ1M6Z2R41Y.public.t_rbaseit limit 10;');
     st.dataframe(query)
 
 
