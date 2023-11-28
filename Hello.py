b2f3e95ef688ed05042056311c4817edd63cf19a
Ayomitunde Fafore <ayomitunde.fafore@snowflake.com>
2023-11-28 09:31:47 -0600

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -29,7 +29,8 @@ def run():
     print("connected to snowflake!")
     cur = conn.cursor()
     # conn = st.experimental_connection("snowpark", private_key=pkb, role="readonly_role")
-    query = cur.execute('select * from free_dataset_GZSNZ2UNRS.public.t_rbaseit limit 10;');
+    cur.execute('use FREE_DATASET_GZT0ZLVIV74');
+    query = cur.execute('select * from FREE_DATASET_GZT0ZLVIV74 limit 10;');
     st.dataframe(query)
 
 
